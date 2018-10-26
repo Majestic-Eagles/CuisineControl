fa9aedada4a74862df2c89a7c6f6a79b04eb27f3
Taylor McLaughlin <taylormclaughlin@dhcp-10-134-227-69.uawifi.arizona.edu>
2018-10-26 14:07:48 -0700

    crashing request

--- CuisineControl/APIManagers/EdamamAPIManager.swift
+++ CuisineControl/APIManagers/EdamamAPIManager.swift
@@ -10,10 +10,15 @@ import Foundation
 
 class EdamamAPIManager {
     
-    let applicationID = "700a667e"
-    let applicationKey = "12c0086d319c5d974cfc2a3182e14058"
+    let foodDatabaseApplicationID = "700a667e"
+    let foodDatabaseApplicationKey = "12c0086d319c5d974cfc2a3182e14058"
+    let searchApplicationID = "832f958b"
+    let searchApplicationKey = "e8cf23abb1138f67a8fcee3299fb2f56"
+    let appID = "&app_id="
+    let appKey = "&app_key="
     let baseURLString = "https://api.edamam.com/"
-    let testString = "search?q=chicken&app_id=832f958b&app_key=e8cf23abb1138f67a8fcee3299fb2f56"
+    let searchString = "search?q=chicken&app_id=832f958b&app_key=e8cf23abb1138f67a8fcee3299fb2f56"
+    let foodDatabaseStringBase = "food-database/parser?ingr="
     var session: URLSession
     static var shared: EdamamAPIManager = EdamamAPIManager()
     
@@ -21,10 +26,14 @@ class EdamamAPIManager {
         session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
     }
     
-    func getData() {
+    func getFoodData(foodString: String) {
         
+        var otherFoodString = foodString
         
-        if let url = URL(string: baseURLString + testString) {
+        otherFoodString = foodString.replacingOccurrences(of: " ", with: "%20")
+        print(otherFoodString)
+        
+        if let url = URL(string: baseURLString + foodDatabaseStringBase + otherFoodString + appID + foodDatabaseApplicationID + appKey + foodDatabaseApplicationKey) {
             let request = URLRequest(url: url)
             let task = session.dataTask(with: request) { (data, response, error) in
                 if let data = data {
