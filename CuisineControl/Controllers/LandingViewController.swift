b344c92248bebacc9f0f11255e251b922c4e7500
Sierra Klix <sierraklix@Sierras-MacBook-Air.local>
2018-12-01 14:32:47 -0700

    added popup buttons on load but the loading screen acts wierd with iphone x

--- CuisineControl/Controllers/LandingViewController.swift
+++ CuisineControl/Controllers/LandingViewController.swift
@@ -12,77 +12,30 @@ class LandingViewController: UIViewController {
     
     @IBOutlet weak var backgroundImageView: UIImageView!
     
+    @IBOutlet weak var buttonsView: UIView!
+    @IBOutlet weak var createAccount: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
     var myGreen = UIColor(red: 71/255, green: 227/255, blue: 40/255, alpha: 1)
-    var signInButtonBottomConstraint: NSLayoutConstraint!
     
-    @objc func handleCreateAccount(sender: UIButton) {
-        
-    }
-    @objc func handleSignIn(sender: UIButton) {
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        set_up()
+        UIView.animate(withDuration: 1, animations: {
+            self.buttonsView.transform = CGAffineTransform(translationX: 0, y: -250)
+        })
         
-        let createAccountButton = UIButton(type: .system)
-        let signInButton = UIButton(type: .system)
-        createAccountButton.addTarget(self, action: #selector(handleCreateAccount), for: .touchUpInside)
+ 
         
-        view.addSubview(createAccountButton)
-        view.addSubview(signInButton)
         
-        createAccountButton.translatesAutoresizingMaskIntoConstraints = false
-        signInButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        createAccountButton.backgroundColor = myGreen
-        createAccountButton.tintColor = UIColor.white
-        createAccountButton.setTitle("Create Account", for: .normal)
-        createAccountButton.layer.cornerRadius = 5
-        signInButton.backgroundColor = UIColor.white
-        signInButton.tintColor = UIColor.darkGray
-        signInButton.setTitle("Sign In", for: .normal)
+    }
+    func set_up(){
+        signInButton.backgroundColor = myGreen
         signInButton.layer.cornerRadius = 5
-        createAccountButton.alpha = 1
-        signInButton.alpha = 1
-        
-        setConstraints(signInButton: signInButton, createAccountButton: createAccountButton)
-        
-        let signInBottomConstraint = signInButton.bottomAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: -30)
-        UIView.animate(withDuration: 50) {
-            self.view.layoutIfNeeded()
-        }
-
-//        UIView.animate(withDuration: 50) {
-//           createAccountButton.center.y = createAccountButton.center.y / (self.view.frame.height / 5)
-//            signInButton.center.y = signInButton.center.y / (self.view.frame.height / 5)
-//        }
-        
-        
-        
-        
+        createAccount.layer.cornerRadius = 5
     }
     
-    func setConstraints(signInButton: UIButton, createAccountButton: UIButton) {
-        
-//        let signInButtonLeftConstraint = NSLayoutConstraint(item: signInButton, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: 12)
-//        let signInButtonRightConstraint = NSLayoutConstraint(item: signInButton, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -12)
-//
-//        createAccountButton.addConstraints(<#T##constraints: [NSLayoutConstraint]##[NSLayoutConstraint]#>)
-        
-        
-        createAccountButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
-        createAccountButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
-        createAccountButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        createAccountButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        createAccountButton.bottomAnchor.constraint(equalTo: signInButton.topAnchor, constant: -8).isActive = true
-        signInButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
-        signInButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
-        signInButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        signInButton.bottomAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: -30).isActive = true
-        
-    }
+    
     
 
 }
